9605387aa0c424be81da0ac42de75504018dd676
adamjohnlea <adamjlea@gmail.com>
2026-04-28 17:15:37 -0600

    Tooling: relax test docblock rules, bump phpstan memory, simplify phpstan exclude paths

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -9,5 +9,10 @@ declare(strict_types=1);
 
 namespace LEAStudios\Tests;
 
+/**
+ * Base test case extending WP_UnitTestCase.
+ *
+ * All plugin tests extend this class.
+ */
 abstract class TestCase extends \WP_UnitTestCase {
 }
